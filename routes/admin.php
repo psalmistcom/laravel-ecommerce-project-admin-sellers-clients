87229ba68e89bb3444239a4c396bf8bd5ed27026
psalmistcom <dadewale9@gmail.com>
2023-06-12 16:58:28 +0100

    admin controller created

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-Route::get('/admin', function(){
-    return 'hello admin';
+Route::prefix('admin')->name('admin')->group(function(){
+    Route::middleware(['guest:admin'])->group(function(){
+        Route::view('/login', 'back.pages.admin.auth.login')->name('login');
+    });
+    Route::middleware(['auth:admin'])->group(function(){
+        Route::view('/home', 'back.pages.admin.home')->name('home');
+    });
 });
