44372a46fa632033b58369909dac739dddb2fd49
psalmistcom <dadewale9@gmail.com>
2023-06-12 11:46:31 +0100

    auth layout

--- resources/views/back/layout/auth-layout.blade.php
+++ resources/views/back/layout/auth-layout.blade.php
@@ -26,6 +26,18 @@
 </head>
 
 <body class="login-page">
+    <div class="pre-loader">
+        <div class="pre-loader-box">
+            <div class="loader-logo">
+                <img src="/back/vendors/images/deskapp-logo.svg" alt="" />
+            </div>
+            <div class="loader-progress" id="progress_div">
+                <div class="bar" id="bar1"></div>
+            </div>
+            <div class="percent" id="percent1">0%</div>
+            <div class="loading-text">Loading...</div>
+        </div>
+    </div>
     <div class="login-header box-shadow">
         <div class="container-fluid d-flex justify-content-between align-items-center">
             <div class="brand-logo">
