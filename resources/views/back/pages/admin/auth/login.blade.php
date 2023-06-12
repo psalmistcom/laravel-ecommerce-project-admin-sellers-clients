57cd4bc172a5996e40458b5f283dfd3456cdec25
psalmistcom <dadewale9@gmail.com>
2023-06-12 17:24:30 +0100

    admin login ui done

--- resources/views/back/pages/admin/auth/login.blade.php
+++ resources/views/back/pages/admin/auth/login.blade.php
@@ -8,19 +8,40 @@
                 <h2 class="text-center text-primary">Admin Login</h2>
             </div>
             <form>
-
+                @if (Session::get('fail'))
+                    <div class="alert alert-danger">
+                        {{ Session::get('fail') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                            <span aria-hidden="true">&times; </span>
+                        </button>
+                    </div>
+                @endif
                 <div class="input-group custom">
-                    <input type="text" class="form-control form-control-lg" placeholder="Username" />
+                    <input type="text" class="form-control form-control-lg" placeholder="Email" name="login_id"
+                        value="{{ old('login_id') }}" />
                     <div class="input-group-append custom">
                         <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
                     </div>
                 </div>
+                @error('login_id')
+                    <div class="d-block text-danger" style="margin-top:-25px; margin-bottom:15px;">
+                        {{ $message }}
+                    </div>
+                @enderror
+
                 <div class="input-group custom">
-                    <input type="password" class="form-control form-control-lg" placeholder="**********" />
+                    <input type="password" class="form-control form-control-lg" placeholder="**********" name="password"
+                        value="{{ old('password') }}" />
                     <div class="input-group-append custom">
                         <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
                     </div>
                 </div>
+                @error('password')
+                    <div class="d-block text-danger" style="margin-top:-25px; margin-bottom:15px;">
+                        {{ $message }}
+                    </div>
+                @enderror
+
                 <div class="row pb-30">
                     <div class="col-6">
                         <div class="custom-control custom-checkbox">
